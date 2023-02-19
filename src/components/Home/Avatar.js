89a4ac316a556fd97d92b2a8876c60e86b44623c
zKittyGB <a-hebert@hotmail.fr>
2023-02-19 19:18:14 +0100

    Updates

--- src/components/Home/Avatar.js
+++
@@ -1,9 +0,0 @@
-import "../../css/home/Avatar.css";
-import imgAvatar from "../../assets/portrait_moi_avatar.jpg";
-export default function Avatar() {
-  return (
-    <div className="avatarArea">
-      <img className="avatarArea_img" src={imgAvatar} alt="avatar" />
-    </div>
-  );
-}
